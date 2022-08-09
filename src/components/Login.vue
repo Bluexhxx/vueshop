f83c04138b1ee92b07a697c5f1215de87bbd3cef
Bluexhxx <Xhcode200@tom.com>
2022-08-09 13:08:54 +0800

    登录功能还未完成

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login_container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b5b6b;
+  height: 100%;
+}
+
+</style>
