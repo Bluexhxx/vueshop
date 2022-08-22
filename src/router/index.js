86e507d01f7a849e9ec1228cc94d20ebee4086ee
Bluexhxx <Xhcode200@tom.com>
2022-08-23 01:23:07 +0800

    添加登录功能以及/home的基本结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '../components/Login'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login";
+import Home from "../components/Home";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login }
-  ]
-})
+    { path: "/", redirect: "/login" },
+    { path: "/login", component: Login },
+    { path: "/home", component: Home },
+  ],
+});
+//挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login") return next();
+
+  //获取token
+  const tokenStr = window.sessionStorage.getItem("token");
+
+  if (!tokenStr) return next("/login");
+
+  next();
+});
+export default router 
